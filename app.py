d985d9b430c2c58c4474ab523903236b4281dbb7
chaitanyagarimella <32245092+chaitanyagarimella@users.noreply.github.com>
2024-08-26 09:25:24 +0000

    Update after demo with new model in app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from langchain_community.llms import Ollama
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
@@ -16,19 +17,20 @@ load_dotenv()
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 
-llm = Ollama(model="qwen:0.5b")  # Use Ollama's qwen:.5b model
+llm = Ollama(model="qwen2:7b")  # Use Ollama's qwen2:7b model
 
 def vector_embedding():
     if "vectors" not in st.session_state:
         st.session_state.embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")  # Use updated HuggingFace Embeddings
         
         # Ensure the file path is correct
-        pdf_path = "./The_Archer.pdf"
-        if not os.path.isfile(pdf_path):
-            st.error(f"File not found: {pdf_path}")
+        pdf_path = "/home/genaiadmin/coauthor/Co-author/docs1"
+        if not os.path.isdir(pdf_path):
+            st.error(f"Directory not found: {pdf_path}")
             return
         
-        st.session_state.loader = PyPDFLoader(pdf_path)  # data ingestion
+        #Load the directory content
+        st.session_state.loader = DirectoryLoader(pdf_path)  # data ingestion
         try:
             st.session_state.docs = st.session_state.loader.load()  # document loading
         except Exception as e:
@@ -41,18 +43,12 @@ def vector_embedding():
             st.error("No documents were loaded. Check the file path and format.")
             return
         
-        # Print the content of the loaded documents
-        for doc in st.session_state.docs:
-            print(doc.page_content[:500])  # Print the first 500 characters of each document for inspection
-        
+               
         # Adjust chunk size and overlap if needed
         st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)  # chunk creation
         st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs)  # splitting
-        print(f"Number of final documents: {len(st.session_state.final_documents)}")  # Debugging line
+        print(f"Number of total pages loaded: {len(st.session_state.final_documents)}")  # Debugging line
         
-        # Print the content of the final split documents
-        for final_doc in st.session_state.final_documents:
-            print(final_doc.page_content[:500])  # Print the first 500 characters of each split document for inspection
         
         if len(st.session_state.final_documents) == 0:
             st.error("No final documents created after splitting. Adjust chunk size and overlap.")
@@ -60,8 +56,23 @@ def vector_embedding():
 
         st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)
 
-st.title("Ollama qwen:0.5b Demo")
+#end of vector embedding function
 
+st.title("Co-author Demo")
+st.markdown("![Alt Text](https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXdpYms2a3V0YThmMnRwaWUwdWxpM2Jld2ZmMDRybzN3ajV5N2lqYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xIytx7kHpq74c/200.webp)")
+initial_message = "Hi, This is Chitti. Speed one Terahertz, Memory one Zettabyte !!! How can I help you?"
+system_message = st.chat_message("assistant")
+system_message.write(initial_message)
+	
+#To store the chat history
+if "messages" not in st.session_state :
+	st.session_state.messages = []
+
+for message in st.session_state.messages :
+	with st.chat_message(message["role"]):
+		st.markdown(message["content"])
+	
+#define the chat prompt template.
 prompt = ChatPromptTemplate.from_template(
 """
 Answer the question based on the provided context only.
@@ -73,22 +84,26 @@ Question:{input}
 """
 )
 
-prompt1 = st.text_input("Enter your Question from Document")
-
-if st.button("Document Embeddings"):
+#Ask the user to enter the question and show the provided question in the chat box for history
+if st.button("Document Embeddings",type="primary"):
     vector_embedding()
     st.write("Vector Store DB is Ready")
+if user_prompt := st.chat_input("Enter your Question from Document") :
+	st.chat_message("user").markdown(user_prompt)
+	st.session_state.messages.append({"role" :"user","content":user_prompt})
 
-if prompt1 and "vectors" in st.session_state:
+if user_prompt and "vectors" in st.session_state:
     document_chain = create_stuff_documents_chain(llm, prompt)
     retriever = st.session_state.vectors.as_retriever()
     retrieval_chain = create_retrieval_chain(retriever, document_chain)
     start = time.process_time()
-    response = retrieval_chain.invoke({"input": prompt1})
+    response = retrieval_chain.invoke({"input": user_prompt})
     print("Response time: ", time.process_time() - start)
-    st.write(response['answer'])
+    #st.write(response['answer'])
+    st.chat_message("assistant").markdown(response['answer'])
+    st.session_state.messages.append({"role":"assistant","content":response['answer']})
 
-    with st.expander("Document similarity search"):
-        for i, doc in enumerate(response["context"]):
-            st.write(doc.page_content)
-            st.write("------------------------------")
+    #with st.expander("Document similarity search"):
+        #for i, doc in enumerate(response["context"]):
+           # st.write(doc.page_content)
+            #st.write("------------------------------")
